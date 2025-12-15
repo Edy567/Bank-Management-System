40ee0668e896ed0bcb7f9e0c66465085fa3dd71b
Edy567 <edwifieraru@yahoo.com>
2025-12-15 23:29:41 +0200

    tema2 si interfata sfml

--- main.cpp
+++ main.cpp
@@ -4,10 +4,8 @@
 #include "InterfataBanca.h"  // Header-ul interfetei grafice
 
 int main() {
-
     std::vector<Client> clienti;
     std::vector<Angajat> angajati;
-
 
 
     Banca myBank("Banking App", clienti, angajati);
